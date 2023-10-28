cf3a0e2c1307966621f2b284b1e69b20d947a04d
Faxraddin <reydivonay@gmail.com>
2023-10-28 20:49:38 +0400

    smm

--- src/layouts/Footer.jsx
+++ src/layouts/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <div style={{backgroundColor:'#252F3D'}} className='w-full h-[88px] relative top-[250px] text-white flex justify-center items-center'>
-        <div className='w-[90%] sm:gap-5 sm:flex-col pm:text-center flex justify-between items-center'>
+        <div className='w-[90%] sm:gap-5 sm:flex-col sm:text-center flex justify-between items-center'>
             <span className='pm:text-[12px]'>Copyright (c) 2023 | Clarifionsupport@clarifion.com</span>
             <img src='/fotter.png' className='w-[231px] sm:w-[172px] sm:h-[12px] h-[16px]'/>
         </div>
